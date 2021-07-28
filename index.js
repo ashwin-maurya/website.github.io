747a0a14e182a9b9a7b2fedbad8b08f1fbf95c84
Ashwin Maurya <ashwinmaurya30@gmail.com>
2021-07-28 21:53:11 +0530

    new
    update

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('My Portfolio');
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {
+
+    if (document.documentElement.scrollTop > 200) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
